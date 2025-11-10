64c66d591e1a755a2a4ad2dfb9b5abec62d54980
b00971548 <b00971548@M-MXB105-C62>
2025-11-10 15:35:50 +0000

    animal class

---
+++ MyInheritanceLibrary/Animal.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyInheritanceLibrary
+{
+    internal class Animal
+    {
+        private string name;
+        private int age;
+        private int numberOfLegs;
+
+
+        
+
+        public string Name
+        {
+            get { return name; }
+            set { name = value; }
+        }
+        public int Age
+        { 
+            get { return age; } 
+            set { age = value; } 
+        }
+
+        public int NumberOfLegs
+        {
+            get { return numberOfLegs; }
+            set { numberOfLegs = value; }
+        }
+
+
+        //methods
+        public void Speak()
+        {
+            Console.WriteLine("Hello");
+        }
+    }
+}
